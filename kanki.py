0079537fc5cf43aa65efe8c6be6cba239ee4b615
William Johnsson <wjohnsson@gmail.com>
2019-11-14 09:49:40 +0100

    Add print of available books

--- kanki.py
+++ kanki.py
@@ -12,17 +12,26 @@ def parse_args():
     return arg_parser.parse_args()
 
 
+def print_books(cursor):
+    """Print all books in database"""
+    cursor.execute("SELECT title FROM BOOK_INFO")
+    # Some books seem to appear multiple times, so take unique
+    books = set([book_name[0] for book_name in cursor.fetchall()])
+
+    print("Books:")
+    for b in books:
+        print("  " + b)
+
+
 def main():
     args = parse_args()
 
+    # Connect to vocabulary database
     connection = sqlite3.connect(args.db_path)
     cursor = connection.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type= 'table'")
-    print(str(cursor.fetchall()) + "\n")
 
     if args.books:
-        cursor.execute("SELECT title FROM BOOK_INFO")
-        print(cursor.fetchall())
+        print_books(cursor)
 
 
 if __name__ == "__main__":
