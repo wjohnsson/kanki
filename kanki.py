376d1c1ea56c035a55ef1d30b11e16ca33cde947
William Johnsson <wjohnsson@gmail.com>
2019-11-17 11:43:23 +0100

    Look up words in Merriam Webster Learners Dictionary
    Results are in json format. My API key is limited to 1000 lookups/day.

--- kanki.py
+++ kanki.py
@@ -1,5 +1,6 @@
 import argparse
 import sqlite3
+import requests
 
 
 def parse_args():
@@ -23,15 +24,37 @@ def print_books(cursor):
         print("  " + b)
 
 
+def lookup_word(word):
+    api_key = "your_api_key_here"
+    api_request = "https://www.dictionaryapi.com/api/v3/references/learners/json/" + word + "?key=" + api_key
+
+    print("Looking up word... " + word)
+    response = requests.get(api_request)
+    print("Status code: " + str(response.status_code))
+    return response.json()
+
+
+def parse_lookup_entry(cursor):
+    """Parses a row in the LOOKUPS table"""
+    cursor.execute("SELECT word_key, usage FROM LOOKUPS")
+    word, sentence = cursor.fetchone()
+
+    # remove 'en:' from word
+    return word[3:], sentence
+
+
 def main():
     args = parse_args()
 
-    # Connect to vocabulary database
+    # Connect to vocabulary database file
     connection = sqlite3.connect(args.db_path)
     cursor = connection.cursor()
 
     if args.books:
         print_books(cursor)
+    else:
+        word, sentence = parse_lookup_entry(cursor)
+        print("Resulting json: " + str(lookup_word(word)))
 
 
 if __name__ == "__main__":
