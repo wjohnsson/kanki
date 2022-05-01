56a1291642b47deace542beb7dee4af1088b1d49
William Johnsson <wjohnsson@gmail.com>
2022-05-01 23:20:29 +0200

    Add test for book with too many lookups

--- tests/test_kanki.py
+++ tests/test_kanki.py
@@ -1,3 +1,4 @@
+import pytest
 import pytest_mock
 
 from kanki.kanki import Kanki
@@ -26,3 +27,16 @@ def test_remove_books_until_safe(mocker: pytest_mock.MockerFixture):
 
     assert expected_1 == actual_1, 'wrong book remaining'
     assert expected_2 == actual_2, 'the book should stay in the list'
+
+
+def test_remove_unsafe_book(mocker: pytest_mock.MockerFixture):
+    kanki = Kanki()
+    kanki.dictionary = MWDictionary('dummy_api_key')
+
+    def mock_count_lookups(self, book_title: str):
+        return MWDictionary.max_queries + 1  # represents an unsafe book
+
+    mocker.patch.object(Kanki, 'count_lookups', mock_count_lookups)
+    with pytest.raises(SystemExit):
+        kanki.remove_books_until_safe(['A'])
+
